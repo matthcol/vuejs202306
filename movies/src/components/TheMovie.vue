8888e9f93da44c228e051f605e2f6a75652a21c7
matthcol <matthias.colin@gmail.com>
2023-07-03 13:22:38 +0200

    first commit

---
+++ movies/src/components/TheMovie.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+defineProps<{
+  title: string,
+  year: number,
+  duration: number
+}>()
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ title }}</h1>
+    <ul>
+      <li>year: {{ year }}</li>
+      <li>duration: {{ duration }}mn</li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
