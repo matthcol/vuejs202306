7975b3087645eb45b509aa331e514dec5a17de8b
matthcol <matthias.colin@gmail.com>
2023-07-03 16:54:09 +0200

    fix decorator usage

--- movies/src/components/MovieCollection.ts
+++ movies/src/components/MovieCollection.ts
@@ -2,7 +2,7 @@
 import { Component, Vue } from 'vue-facing-decorator'
 import type { Movie } from "./Movie"
 
-@Component
+@Component({})
 export default class MovieCollection extends Vue {
 
     movies: Movie[] = [];
