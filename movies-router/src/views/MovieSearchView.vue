764c2e45297b256cfae269fb271a93f41540a903
matthcol <matthias.colin@gmail.com>
2023-07-05 06:24:02 +0200

    remove dead code

--- movies-router/src/views/MovieSearchView.vue
+++ movies-router/src/views/MovieSearchView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, type Ref } from "vue";
+import { ref, watch, type Ref } from "vue";
 import { useRoute } from "vue-router";
 import router from "../router";
 import LifeCycle from "@/components/LifeCycle.vue";
@@ -12,34 +12,20 @@ const year: Ref<number | null> = ref(null);
 
 console.log("Query params setup:", route.query);
 
+const queryParams = ref(route.query);
+
 const changeQueryparams = () => {
   console.log("change query params");
   router.replace({ name: "search", query: { t: "Rambo", y: 1982 } });
 };
+
+// TODO: to finsih ...
+// Article with a lot of examples:
+//   https://zelig880.com/how-to-use-watch-in-vue-3-in-composition-api-and-script-setup
+watch(queryParams, (newQueryParams) =>
+  console.log("Query params changed:", newQueryParams)
+);
 </script>
-<!-- <script lang="ts">
-import { ref, type Ref } from "vue";
-
-export default {
-  setup() {
-    const title: Ref<string | null> = ref(null);
-    const year: Ref<number | null> = ref(null);
-    const changeTitle = () =>  router.push({ name: 'search', params: { y: 'E.T.' }})
-  },
-
-  created() {
-    console.log("data:", this.$data);
-    console.log("title:", this.title);
-    console.log("year:", this.year);
-    this.$watch(
-      () => this.$route.query,
-      (toQuery, previousQuery) => {
-        console.log("Query params changed:", toQuery);
-      }
-    );
-  },
-};
-</script> -->
 
 <template>
   <div>
